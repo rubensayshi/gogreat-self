1c84150ade9044cb35aec660844dbe0e37e8db34
Ruben de Vries <rubensayshi@gmail.com>
2011-04-10 12:52:49 +0000

    now using .php for parameters in config so as first step to making them dynamic based on env

---
+++ app/config/parameters.php
@@ -0,0 +1,14 @@
+<?php
+
+$container->setParameter('database_driver', 	'pdo_mysql');
+$container->setParameter('database_host',		'localhost');
+$container->setParameter('database_name', 		'symfony');
+$container->setParameter('database_user', 		'root');
+$container->setParameter('database_password', 	'root');
+$container->setParameter('mailer_transport', 	'smtp');
+$container->setParameter('mailer_host', 		'localhost');
+$container->setParameter('mailer_user', 		'');
+$container->setParameter('mailer_password', 	'');
+$container->setParameter('locale', 				'en');
+$container->setParameter('csrf_secret', 		'xxxxxxxxxx');
+
