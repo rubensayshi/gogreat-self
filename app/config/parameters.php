1096f723c94ca46af613bfc9a67b487cee13819b
Ruben de Vries <rubensayshi@gmail.com>
2011-04-10 13:49:34 +0000

    through parameters.php we now use the cnfs defined in the apps

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,14 +1,18 @@
 <?php
 
-$container->setParameter('database_driver', 	'pdo_mysql');
-$container->setParameter('database_host',		'localhost');
-$container->setParameter('database_name', 		'symfony');
-$container->setParameter('database_user', 		'root');
-$container->setParameter('database_password', 	'root');
+use GoGreat\SymfonyWrapper\SymfonyCnf;
+
+if($database = SymfonyCnf::getSection('database'))
+	foreach($database as $key => $val)
+		if($key = preg_replace('/^symfony\.db/', 'database', $key))
+			$container->setParameter($key,		trim($val));
+			
 $container->setParameter('mailer_transport', 	'smtp');
 $container->setParameter('mailer_host', 		'localhost');
 $container->setParameter('mailer_user', 		'');
 $container->setParameter('mailer_password', 	'');
+
 $container->setParameter('locale', 				'en');
+
 $container->setParameter('csrf_secret', 		'xyxyxyxyxyxy');
 
