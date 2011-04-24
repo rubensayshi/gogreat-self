8729e44000a3be68fbdd115b5f4b9039a8c17760
= <=>
2011-04-24 18:09:22 +0000

    no fixture data yet

--- src/GoGreat/CMSBaseBundle/DataFixtures/ORM/block.php
+++ src/GoGreat/CMSBaseBundle/DataFixtures/ORM/block.php
@@ -9,37 +9,14 @@ use	Doctrine\Common\DataFixtures\FixtureInterface;
 use GoGreat\CMSBaseBundle\Entity;
 
 
-class LoadCMSBasePageData extends ContainerAware implements FixtureInterface
+class LoadCMSBaseBlockData extends ContainerAware implements FixtureInterface
 {
 	public function load($manager)
 	{
-		$menuCnt = 0;
-		for($p = 0; $p < 10; $p++)
-		{
-			$page = new Entity\Page();
-			$page->setTitle("lôrém tl #{$p}");
-			$page->setContent("lorem tl ipsum #{$p}");
 
-			$manager->persist($page);	
-			
-			if(round(mt_rand(0, 1)) && $menuCnt < 3)
-			{
-				$item = new Entity\MenuItem();
-				$item->setTitle($page->getTitle());
-				$item->setRouting('page');
-				$item->setArguments(array('slug' => $page->getSlug()));
-				$item->setIdentifier($page->getMenuIdentifier());
-	
-				$manager->getRepository('GoGreat\CMSBaseBundle\Entity\MenuItem')->persist($item);
-				$menuCnt++;
-			}
-					
-		}
-
-		$manager->flush();
 	}
 }
 
 
 $loader = new Loader();
-$loader->addFixture(new LoadCMSBasePageData);
+$loader->addFixture(new LoadCMSBaseBlockData);
