89c1815a3b29e43d8e0f4b79830e6aa12e150fc2
= <=>
2011-05-28 14:13:48 +0000

    add overview and edit page

--- src/GoGreat/SiteManagerBundle/Controller/SiteManagerController.php
+++ src/GoGreat/SiteManagerBundle/Controller/SiteManagerController.php
@@ -10,27 +10,50 @@ class SiteManagerController extends BaseController
 {
     public function newAction()
     {   	
+    	return $this->editAction();
+    }
+    
+    public function editAction($id=null)
+    {   	
     	$form 		= $this->get('form.factory')->create(new WebsiteType());
 		$request 	= $this->get('request');
-		$website	= new Website();
+		
+		$website = ($id) ? $this->getEntityManager()
+    						->getRepository('GoGreat\ServerBaseBundle\Entity\Website')
+    						->findOneById($id) : false;
+    				
+    	if(!$website)
+			$website = new Website();
+			
 		$form->setData($website);
 		
 		if ($request->getMethod() == 'POST') {
 			$form->bindRequest($request);
 
 			if ($form->isValid()) {
+				if(!$website->getUser())
+					$website->setUser($this->getLoggedInUser());
+				
 				$this->getEntityManager()->persist($website);
 				$this->getEntityManager()->flush();
+				
+				return $this->redirect($this->generateUrl('site_overview'));
 			}
 		}
 		
         return $this->render('SiteManagerBundle:SiteManager:new.html.twig', array(
-				'form'		=> $form->createView(),
+				'form'			=> $form->createView(),
 		));
     }
     
     public function overviewAction()
     {
-    	
+    	$websites = $this->getEntityManager()
+    						->getRepository('GoGreat\ServerBaseBundle\Entity\Website')
+    						->findByUser($this->getLoggedInUser()->getUsername());
+		
+        return $this->render('SiteManagerBundle:SiteManager:overview.html.twig', array(
+				'websites'		=> $websites,
+		));
     }
 }
